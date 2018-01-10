7841b5dc1342c091300cf7d2d8a639517babf244
bcollins0905 <b.w.collinssf@gmail.com>
2018-01-09 16:50:26 -0800

    working?

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var MONGODB_URI = require('dotenv').config();
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
 // It works on the client and on the server

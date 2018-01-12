aefd89218159b61e862426bfd0f502f93572b057
bcollins0905 <b.w.collinssf@gmail.com>
2018-01-12 03:35:44 -0800

    debugging

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.get("/scrape", function(req, res) {
         .update(result)
         .then(function(dbArticle) {
           // If we were able to successfully scrape and save an Article, send a message to the client
-          res.send("Scrape Complete");
+          res.send(db.Article);
         })
         .catch(function(err) {
           // If an error occurred, send it to the client
